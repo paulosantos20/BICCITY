0d2f0bd7fe4a9f3f37f586f6414d30eab3cd59e5
paulosantos20 <ph_paulito@hotmail.com>
2017-06-10 22:06:21 -0300

    Backup_10_06_17
    Varias telas retiradas, refazer todas!!
    Tab moeda e cupons da tela de usuario, sendo refeita...

--- aplicativo/BICCITY/www/js/inicio.js
+++ aplicativo/BICCITY/www/js/inicio.js
@@ -13,7 +13,7 @@ function initial() {
                $("#msg").html("Fazendo login e entrando!!");
                $("#nuser").html("paulo!!");
                clearInterval(id);
-               activate_page("#esplaz"); 
+               activate_page("#user"); 
                 
                return false;
               
