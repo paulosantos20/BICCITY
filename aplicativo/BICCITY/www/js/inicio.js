4585acdae15b81528ee2c6af09f872216faac00c
paulosantos20 <ph_paulito@hotmail.com>
2017-08-08 22:58:00 -0300

    Backup_08_08_17
    3 novas telas adicionadas!!
    1 tela 100% concluida....

--- aplicativo/BICCITY/www/js/inicio.js
+++ aplicativo/BICCITY/www/js/inicio.js
@@ -15,7 +15,7 @@ function initial() {
                clearInterval(id);
               
           //     activate_page("#user"); 
-              activate_page("#regulamento"); 
+              activate_page("#exchangecoupon"); 
                 
                return false;
               
